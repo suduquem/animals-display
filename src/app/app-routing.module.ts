657a92b608e7a554a7219b1374ff64060d1f1798
Susana Duque <su.duquem@gmail.com>
2020-09-28 20:09:51 -0500

    Se crea el componente welcome y se configuran las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ActivatedRoute, ParamMap } from '@angular/router';
 
 import { DogsComponent } from './animals/components/dogs/dogs.component';
 import { CatsComponent } from './animals/components/cats/cats.component';
 import { BirdsComponent } from './animals/components/birds/birds.component';
+import { WelcomeComponent } from './shared/components/welcome/welcome.component';
 
 const routes: Routes = [
-  {path: '', component: DogsComponent },
+  {path: '', component: WelcomeComponent},
+  {path: 'dogs', component: DogsComponent },
   {path: 'cats', component: CatsComponent },
   {path: 'birds', component: BirdsComponent }
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
-  declarations: [CardComponent, HeaderComponent, NavigationComponent],
-  imports: [CommonModule],
-  exports: [CardComponent, HeaderComponent, NavigationComponent],
+  declarations: [CardComponent, HeaderComponent, NavigationComponent, WelcomeComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [CardComponent, HeaderComponent, NavigationComponent, WelcomeComponent],
 })
 export class SharedModule {}
